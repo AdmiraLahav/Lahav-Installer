ef87c709828994058bc53ec3f391a97507f53ad7
AdmiraLahav <113438937+AdmiraLahav@users.noreply.github.com>
2025-12-23 20:55:51 +0200

    Update Cloner.py

--- Cloner.py
+++ Cloner.py
@@ -1,10 +1,10 @@
 import subprocess
 
-repo_url = "https://github.com/AdmiraLahav/AdmiraLahav.github.io.git"
+repo_url = input("Input url")
 target_dir = "cloned_repo"
-
+#git clone the repo
 subprocess.run([
     "git", "clone", repo_url, target_dir
 ], check=True)
-
+#open explorer and change dir to the cloned repo
 subprocess.run(["explorer.exe",f"{target_dir}"])
