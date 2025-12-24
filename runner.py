0b8b5f130f854b91fb4d288a76dd339d33f82969
AdmiraLahav <113438937+AdmiraLahav@users.noreply.github.com>
2025-12-24 17:15:48 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -3,6 +3,8 @@ import sys
 
 def launch():
     #sys.executable lets the program run python files
-    subprocess.Popen([sys.executable, "PRINT.py"])
+    A=subprocess.Popen([sys.executable, "PRINT.py"])
+    return A
+
 if __name__ == '__main__':
     launch()
