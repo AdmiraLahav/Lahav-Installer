2431a8026321a09cab700460e04c75131404efc1
AdmiraLahav <113438937+AdmiraLahav@users.noreply.github.com>
2025-12-24 18:48:54 +0200

    rename

--- txt_compression/compressor.py
+++ txt_compression/compressor.py
@@ -1,7 +1,7 @@
 import lzma
 import base64
 
-with open("login.py", "rb") as f:
+with open("running_txt.py", "rb") as f:
     raw = f.read()
 
 packed = base64.b85encode(lzma.compress(raw)).decode("ascii")
